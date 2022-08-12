a6e6e13200da8baf30ef0a2e74bdf95708c9e44d
jazzypants1989 <jessepence@gmail.com>
2022-08-11 23:48:15 -0700

    starting to come together

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require ('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 
 // Load config file
@@ -8,6 +10,19 @@ dotenv.config({ path: './config/config.env' })
 connectDB()
 
 const app = express()
+
+// I'm watching you!!
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+//I can ride my bike with no...
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main'
+}
+    ))
+app.set('view engine', '.hbs')
 
 const PORT = process.env.PORT || 5500
 
