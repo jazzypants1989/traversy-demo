38693c5bfa6783b79c9a0829c5da7589dba19a38
jazzypants1989 <jessepence@gmail.com>
2022-08-12 22:51:57 -0700

    finally got the middleware working

--- models/User.js
+++ models/User.js
@@ -22,7 +22,7 @@ const UserSchema = new mongoose.Schema({
     },
     createdAt: {
         type: Date,
-        defatulte: Date.now
+        default: Date.now
     }
 })
 
--- routes/auth.js
+++ routes/auth.js
@@ -6,11 +6,23 @@ const router = express.Router()
 //@route GET /auth/google
 router.get('/google', passport.authenticate ('google', { scope: ['profile'] }))
 
-// @desc auth callback
+// @desc Google auth callback
 // @route GET /auth/google/callback
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
     res.redirect('/dashboard')
 }
 )
+
+//@desc Logout user
+//@route /auth/logout
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function(err) {
+        if (err) {return next(err)}
+            res.redirect('/')
+    })
+})
+
+// changed to async from traversy
 
 module.exports = router
