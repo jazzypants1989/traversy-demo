bac62f0277edaa4c83acfe0f1c4f6ac914b33fbb
jazzypants1989 <jessepence@gmail.com>
2022-08-14 18:59:17 -0700

    edit routes almost finished

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require ('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require ('method-override')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
@@ -22,6 +23,16 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+// Method Override
+app.use(methodOverride(function (req, res) {
+    if(req.body && typeof req.body === 'objecet' && '_method' in req.body) {
+        //look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
+
 // I'm watching you!!
 
 if (process.env.NODE_ENV === 'development') {
@@ -29,7 +40,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Hamburger Helpers
-const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon, select} = require('./helpers/hbs')
 
 // I can ride my bike with no...
 app.engine('.hbs', exphbs.engine({
@@ -38,6 +49,7 @@ app.engine('.hbs', exphbs.engine({
         stripTags,
         truncate,
         editIcon,
+        select
     },
     defaultLayout: 'main',
     extname: '.hbs'
